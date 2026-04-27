3f04de989adaf750dc953e5271c71f09ecab7c0c
lennymethu-design <lennymethu@gmail.com>
2026-04-27 11:29:53 +0300

    Initial commit - DARKLINE X FIESTA app

---
+++ db.js
@@ -0,0 +1,12 @@
+// DARKLINE X FIESTA — Realtime Database config
+// Import this in any page that needs Firebase
+
+export const firebaseConfig = {
+  apiKey:"AIzaSyAC2ctWv8fu06DNYFzeT76pote-KUFbxfA",
+  authDomain:"darkline-x-fiesta.firebaseapp.com",
+  projectId:"darkline-x-fiesta",
+  storageBucket:"darkline-x-fiesta.firebasestorage.app",
+  messagingSenderId:"22306597234",
+  appId:"1:22306597234:web:c11a440fff8b284d3795eb",
+  databaseURL:"https://darkline-x-fiesta-default-rtdb.firebaseio.com"
+};
